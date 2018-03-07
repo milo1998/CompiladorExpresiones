2a5ab126dbb8d636a5a3c1b453bd431db87f09bf
a52290451 <36229498+a52290451@users.noreply.github.com>
2018-03-06 21:33:47 -0500

    Add files via upload

--- compiler.py
+++ compiler.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
 from pila import Pila
+
+operadores=['*','+','-','/','=']
+
+#Pruebas de validacion de carácteres
+def esVariable (caracter):
+    if caracter.isupper():
+       # print(caracter)
+        if caracter.isalpha():
+            #print("bien")
+            return True
+    return False
+
+def esNumero (caracter):
+    try:
+        caracter = float(caracter)
+        return True
+    except ValueError:
+        return False
+    
+def esOperador(caracter):
+    for x in operadores:
+        if caracter==x:
+            return True
+    return False
+
+#Inicio del proceso
 class Elemento:
     pilaC= Pila()
     dic=[]
@@ -7,31 +33,42 @@ class Elemento:
     i=0
     for x in listaC:
         for element in x:
-            for x in dic:
-                    for y in x:
-                        if y==element:
-                            element=x[1]
-            if (element == '+' or element == '-' or element == '*'
-                or element == '/'):
-                puntder=float(pilaC.desapilar())
-                puntizq=float(pilaC.desapilar())
-                if element=='+':
-                    pilaC.apilar(puntizq + puntder)
-                if element=='-':
-                    pilaC.apilar(puntizq - puntder)
-                if element=='*':
-                    pilaC.apilar(puntizq * puntder)
-                if element=='/':
-                    pilaC.apilar(puntizq / puntder)  
+            if esVariable(element)==True or esNumero(element)==True or esOperador(element)==True:
+                #print ("entro")
+                for s in dic:
+                        for y in s:
+                            if y==element:
+                                element=s[1]
+                if (element == '+' or element == '-' or element == '*'
+                    or element == '/'):
+                    puntder=float(pilaC.desapilar())
+                    puntizq=float(pilaC.desapilar())
+                    if element=='+':
+                        pilaC.apilar(puntizq + puntder)
+                    if element=='-':
+                        pilaC.apilar(puntizq - puntder)
+                    if element=='*':
+                        pilaC.apilar(puntizq * puntder)
+                    if element=='/':
+                        pilaC.apilar(puntizq / puntder)  
+                else:
+                    #print (element)
+                    if(element == '='):
+                        m=pilaC.desapilar()
+                        n=pilaC.desapilar()
+                        print m , " = ", n
+                        
+                        dic.append([m,n])
+                    if(element != '='):
+                        pilaC.apilar(element)
+
             else:
-                #print (element)
-                if(element == '='):
-                    m=pilaC.desapilar()
-                    n=pilaC.desapilar()
-                    print m
-                    print n
-                    dic.append([m,n])
-                if(element != '='):
-                    pilaC.apilar(element)
+                print "El caracter (", element , ") No se reconoce" 
+                break
+        else:
+            continue
+        break
+        
+
                 
-    print (dic)
+    #print (dic)
